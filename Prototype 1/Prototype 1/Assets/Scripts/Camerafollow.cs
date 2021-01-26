593ff2d3ab53c4b4ff89e393b67b2f75a3d79d18
Jacob Steed <10921985@my.uvu.edu>
2021-01-25 17:11:10 -0700

    Added Movement Script and CameraFollow Script

---
+++ Prototype 1/Prototype 1/Assets/Scripts/Camerafollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0,7,-11);
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Prototype 1/Prototype 1/Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Access Modyfier, Data Type, 
+    public float speed = 5.0f;
+
+    public float hInput;
+    public float fInput;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        fInput = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime *speed * fInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * hInput);
+    }
+}
