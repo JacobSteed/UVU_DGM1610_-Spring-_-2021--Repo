ef9cecb1b5014156e3b3433cec17d72dc818cf35
Jacob Steed <10921985@my.uvu.edu>
2021-01-27 16:39:12 -0700

    Added turning code, cleaned up code

--- Prototype 1/Prototype 1/Assets/Scripts/Camerafollow.cs
+++ Prototype 1/Prototype 1/Assets/Scripts/Camerafollow.cs
@@ -7,12 +7,6 @@ public class Camerafollow : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0,7,-11);
    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
--- Prototype 1/Prototype 1/Assets/Scripts/Move.cs
+++ Prototype 1/Prototype 1/Assets/Scripts/Move.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     // Access Modyfier, Data Type, 
-    public float speed = 5.0f;
+    public float speed = 30.0f;
+    public float turnSpeed = 20.0f;
 
     public float hInput;
     public float fInput;
@@ -19,10 +20,12 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Gathers the inputs for controls
         hInput = Input.GetAxis("Horizontal");
         fInput = Input.GetAxis("Vertical");
-        
+        //Makes vehicle go forward and back
         transform.Translate(Vector3.forward * Time.deltaTime *speed * fInput);
-        transform.Translate(Vector3.right * Time.deltaTime * speed * hInput);
+        //Makes vehicle go left and right
+        transform.Rotate(Vector3.up, turnSpeed * hInput * Time.deltaTime);
     }
 }
