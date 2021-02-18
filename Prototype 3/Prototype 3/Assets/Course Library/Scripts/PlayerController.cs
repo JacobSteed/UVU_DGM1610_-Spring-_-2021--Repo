69d4b5aea081f16d719a56c4b1cb54c60b21299b
Jacob Steed <10921985@my.uvu.edu>
2021-02-17 20:41:32 -0700

    Created prototype 3, added gravity mod and jumping player control

---
+++ Prototype 3/Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityMod;
+    public bool isOnGround = true;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gravity Mod and Jump Force
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    
+    // Collision to prevent double jumping
+    private void OnCollisionEnter(Collision other)
+    {
+        isOnGround = true;
+    }
+}
