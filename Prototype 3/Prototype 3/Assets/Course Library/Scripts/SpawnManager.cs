4be49ee2bc532a318be27abb736c12f30d7549a3
Jacob Steed <10921985@my.uvu.edu>
2021-02-24 17:12:08 -0700

    Created Spawn Manager and prefab destroyer.

---
+++ Prototype 3/Prototype 3/Assets/Course Library/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    //Type: Spawn
+    public GameObject obstaclePrefab;
+    
+    //Spawn position
+    private Vector3 spawnPos = new Vector3(25,0,0);
+    
+    //Star Delay interval for invoking
+    public float startDelay = 2.0f;
+    
+    //Repeat Rate interval for invoking
+    public float repeatRate = 2.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
