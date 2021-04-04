8eb30e2415790599c3cf4d796dbd63ce72a400c5
Jacob Steed <10921985@my.uvu.edu>
2021-04-04 00:13:40 -0600

    Cleaned unused space

--- Prototype 4/Prototype 4/Assets/Scripts/PowerUpSpin.cs
+++ Prototype 4/Prototype 4/Assets/Scripts/PowerUpSpin.cs
@@ -5,12 +5,7 @@ using UnityEngine;
 public class PowerUpSpin : MonoBehaviour
 {
     public float speed = 5.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
