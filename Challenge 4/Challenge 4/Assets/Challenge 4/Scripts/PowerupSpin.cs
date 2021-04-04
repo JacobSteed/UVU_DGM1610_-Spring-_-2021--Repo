644ee6deb6c34a861b8ee28c6e6a8a3fb060e179
Jacob Steed <10921985@my.uvu.edu>
2021-04-04 00:10:26 -0600

    Fixed various errors in scripts
    Enemies are now pushed away from player when hit
    powerup now has cooldown
    powerup spawns after each round
    2 new enemies are spawned each round
    enemies gravitate towards player goal

---
+++ Challenge 4/Challenge 4/Assets/Challenge 4/Scripts/PowerupSpin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupSpin : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(speed * 0, 2, 0);
+    }
+}
